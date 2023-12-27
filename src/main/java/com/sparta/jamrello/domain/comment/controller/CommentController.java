426933a8683162fc746d7fea7af867f8638f9556
문정현 <foqlzm12345@gmail.com>
2023-12-27 23:19:46 +0900

    #10 refactor : Comment
    responseEntity 형식 팀원들과 통일

--- src/main/java/com/sparta/jamrello/domain/comment/controller/CommentController.java
+++ src/main/java/com/sparta/jamrello/domain/comment/controller/CommentController.java
@@ -1,16 +1,15 @@
 package com.sparta.jamrello.domain.comment.controller;
 
 import com.sparta.jamrello.domain.comment.dto.CommentRequestDto;
+import com.sparta.jamrello.domain.comment.dto.CommentResponseDto;
 import com.sparta.jamrello.domain.comment.repository.entity.Comment;
-import com.sparta.jamrello.domain.comment.service.CommentService.CommentService;
+import com.sparta.jamrello.domain.comment.service.CommentService;
 import com.sparta.jamrello.domain.member.repository.entity.Member;
 import com.sparta.jamrello.global.constant.ResponseCode;
 import com.sparta.jamrello.global.dto.BaseResponse;
-import com.sparta.jamrello.global.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,7 +24,7 @@ public class CommentController {
     private final CommentService commentService;
 
     @PostMapping("/cards/{cardId}/comment")
-    public ResponseEntity<BaseResponse<Comment>> createComment(
+    public ResponseEntity<BaseResponse<CommentResponseDto>> createComment(
         @RequestBody CommentRequestDto commentRequestDto,
 //        @AuthUser Member member
         @PathVariable Long cardId) {
@@ -33,9 +32,11 @@ public class CommentController {
 //        Member member = userDetails.getMember();
         Member member = new Member("testUser", "password", "nickname", "email@email.com");
 
-        Comment commemt = commentService.createComment(member.getId(), cardId, commentRequestDto);
+        CommentResponseDto commentResponseDto = Comment.toCreateCommentResponse(
+            commentService.createComment(member.getId(), cardId, commentRequestDto).getContent());
 
-        return ResponseEntity.status(HttpStatus.OK).body(BaseResponse.of(ResponseCode.CREATED_COMMENT, commemt));
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(BaseResponse.of(ResponseCode.CREATED_COMMENT, commentResponseDto));
     }
 
 
