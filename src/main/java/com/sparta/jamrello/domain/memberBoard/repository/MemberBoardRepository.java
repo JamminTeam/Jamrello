5801c93aa175a7b1d22baa5b95fbd8c1cb38206e
dmlal <opkl8781@naver.com>
2023-12-27 10:31:01 +0900

    refactor :  memberBoard 수정 및 이동

---
+++ src/main/java/com/sparta/jamrello/domain/memberBoard/repository/MemberBoardRepository.java
@@ -0,0 +1,13 @@
+package com.sparta.jamrello.domain.memberBoard.repository;
+
+import com.sparta.jamrello.domain.board.entity.Board;
+import com.sparta.jamrello.domain.member.repository.entity.Member;
+import com.sparta.jamrello.domain.memberBoard.entity.MemberBoard;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberBoardRepository extends JpaRepository<MemberBoard, Long> {
+
+
+  Optional<MemberBoard> findByMemberAndBoard(Member member, Board board);
+}
