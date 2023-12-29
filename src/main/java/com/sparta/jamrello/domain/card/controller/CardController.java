9df08e6dc3898fb43a7a07a656460f2c41bbee67
Kim Jihyun <legojhk@gmail.com>
2023-12-29 14:53:42 +0900

    refactor(card) : @AuthUser 수정

--- src/main/java/com/sparta/jamrello/domain/card/controller/CardController.java
+++ src/main/java/com/sparta/jamrello/domain/card/controller/CardController.java
@@ -6,14 +6,14 @@ import com.sparta.jamrello.domain.card.dto.request.CardPositionRequestDto;
 import com.sparta.jamrello.domain.card.dto.request.CardRequestDto;
 import com.sparta.jamrello.domain.card.dto.response.CardResponseDto;
 import com.sparta.jamrello.domain.cardCollaborators.dto.CardCollaboratorRequestDto;
+import com.sparta.jamrello.domain.member.repository.entity.Member;
+import com.sparta.jamrello.global.annotation.AuthUser;
 import com.sparta.jamrello.global.constant.ResponseCode;
 import com.sparta.jamrello.global.dto.BaseResponse;
-import com.sparta.jamrello.global.security.UserDetailsImpl;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -33,11 +33,10 @@ public class CardController {
     @PostMapping
     public ResponseEntity<BaseResponse<CardResponseDto>> createCard(
         @PathVariable Long catalogId,
-        @AuthenticationPrincipal UserDetailsImpl userDetails,   // @AuthUser 추후 수정
+        @AuthUser Member member,
         @RequestBody CardRequestDto requestDto) {
 
-        CardResponseDto responseDto = cardService.createCard(catalogId,
-            userDetails.getMember().getId(), requestDto);
+        CardResponseDto responseDto = cardService.createCard(catalogId, member.getId(), requestDto);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(
             BaseResponse.of(ResponseCode.CREATED_CARD, responseDto)
@@ -71,11 +70,10 @@ public class CardController {
     public ResponseEntity<BaseResponse<CardResponseDto>> updateCard(
         @PathVariable Long catalogId,
         @PathVariable Long cardId,
-        @AuthenticationPrincipal UserDetailsImpl userDetails,   // @AuthUser 추후 수정
+        @AuthUser Member member,   // @AuthUser 추후 수정
         @RequestBody CardRequestDto requestDto) {
 
-        CardResponseDto responseDto = cardService.updateCard(cardId,
-            userDetails.getMember().getId(), requestDto);
+        CardResponseDto responseDto = cardService.updateCard(cardId, member.getId(), requestDto);
 
         return ResponseEntity.status(HttpStatus.OK).body(
             BaseResponse.of(ResponseCode.UPDATE_CARD, responseDto)
@@ -86,9 +84,9 @@ public class CardController {
     public ResponseEntity<BaseResponse<String>> deleteCard(
         @PathVariable Long catalogId,
         @PathVariable Long cardId,
-        @AuthenticationPrincipal UserDetailsImpl userDetails   // @AuthUser 추후 수정
+        @AuthUser Member member   // @AuthUser 추후 수정
     ) {
-        cardService.deleteCard(cardId, userDetails.getMember().getId());
+        cardService.deleteCard(cardId, member.getId());
 
         return ResponseEntity.status(HttpStatus.OK).body(
             BaseResponse.of(ResponseCode.DELETE_CARD, "")
@@ -99,13 +97,13 @@ public class CardController {
     public ResponseEntity<BaseResponse<String>> addCollaborator(
         @PathVariable Long catalogId,
         @PathVariable Long cardId,
-        @AuthenticationPrincipal UserDetailsImpl userDetails,
+        @AuthUser Member member,
         @RequestBody CardCollaboratorRequestDto requestDto
     ) {
-        cardService.addCollaborator(cardId, userDetails.getMember().getId(), requestDto);
+        cardService.addCollaborator(cardId, member.getId(), requestDto);
 
         return ResponseEntity.status(HttpStatus.OK).body(
-            BaseResponse.of(ResponseCode.ADD_USER, "")
+            BaseResponse.of(ResponseCode.INVITE_USER, "")
         );
     }
 
@@ -114,9 +112,9 @@ public class CardController {
         @PathVariable Long catalogId,
         @PathVariable Long cardId,
         @PathVariable Long collaboratorId,
-        @AuthenticationPrincipal UserDetailsImpl userDetails
+        @AuthUser Member member
     ) {
-        cardService.deleteCollaborator(cardId, collaboratorId, userDetails.getMember().getId());
+        cardService.deleteCollaborator(cardId, collaboratorId, member.getId());
 
         return ResponseEntity.status(HttpStatus.OK).body(
             BaseResponse.of(ResponseCode.DELETE_USER, "")
@@ -127,10 +125,10 @@ public class CardController {
     public ResponseEntity<BaseResponse<String>> changeCardCatalog(
         @PathVariable Long catalogId,
         @PathVariable Long cardId,
-        @AuthenticationPrincipal UserDetailsImpl userDetails,
+        @AuthUser Member member,
         @RequestBody CardCatalogRequestDto requestDto
     ) {
-        cardService.changeCardCatalog(cardId, userDetails.getMember().getId(), requestDto);
+        cardService.changeCardCatalog(cardId, member.getId(), requestDto);
 
         return ResponseEntity.status(HttpStatus.OK).body(
             BaseResponse.of(ResponseCode.MOVE_CARD_POSITION, "")
@@ -141,10 +139,10 @@ public class CardController {
     public ResponseEntity<BaseResponse<String>> updateCardPos(
         @PathVariable Long catalogId,
         @PathVariable Long cardId,
-        @AuthenticationPrincipal UserDetailsImpl userDetails,
+        @AuthUser Member member,
         @RequestBody CardPositionRequestDto requestDto
     ) {
-        cardService.updateCardPos(catalogId, cardId, userDetails.getMember().getId(), requestDto);
+        cardService.updateCardPos(catalogId, cardId, member.getId(), requestDto);
 
         return ResponseEntity.status(HttpStatus.OK).body(
             BaseResponse.of(ResponseCode.MOVE_CARD_POSITION, "")
